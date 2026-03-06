2d419c331ddad194635688e4032c7ed77bb87f14
Luna Rulle <luna.rulle@pm.me>
2026-03-06 18:41:23 +0200

    added simple python program

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello, world!")
+print("What is your name?")
+name = input()
+print(f"What a start, {name}")
+if name == "Steve":
+    print("Goodbye, world!")
+else:
+    print (f"Goodbye, {name}")
+
